0620aff3a1c90d805e8135f337828b6d4b22fffb
Lucas Rodrigues Alves <102196411+lucasroves@users.noreply.github.com>
2024-08-13 11:46:04 -0300

    Criação de repositório

---
+++ app_financeiro/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, 'home.html')
+
+def resumo(request):
+    return render(request, 'resumo.html')
+
+def despesas(request):
+    return render(request, 'despesas.html')
+
+def vendas(request):
+    return render(request, 'vendas.html')
